76b814c31f6c9851e8839318d1463f991ee03584
ruthmesfun <ruth.mesfun@gmail.com>
2018-11-24 22:06:52 -0500

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :students, :school_classes, only: [:new, :create, :show, :edit, :update]
+  
+
+  # get '/patients/:id', to: 'patients#show', as: 'patient'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
